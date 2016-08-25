1f5c85054d0738f90269a2346499d1997402fcf5
Jody Phelan <jody.phelan@lshtm.ac.uk>
2016-08-25 22:02:33 +0100

    init

--- parseDists_d3.pl
+++ parseDists_d3.pl
@@ -5,14 +5,14 @@ use warnings;
 use List::Util 'reduce';
 sub minindex { my @x=@_; reduce { $x[$a] < $x[$b] ? $a : $b } 0..$#_ }
 
-if (scalar @ARGV < 3){
-	print "\nparseDists_viva.pl <dist_matrix> <minDist> <meta>\n\n";exit;
+if (scalar @ARGV < 2){
+	print "\nparseDists_viva.pl <dist_matrix> <meta>\n\n";exit;
 }
 
 my %hasMeta;
 my %meta;
 my $ct = 0;
-open F, $ARGV[2] or die;
+open F, $ARGV[1] or die;
 while(<F>){
 	my ($sample,$meta) = split /\s+/,$_;
 	if (!exists($hasMeta{$meta})){
@@ -30,7 +30,6 @@ my %dists;
 my @samples;
 my $x=0;
 my $i=0;
-my $maxDist = $ARGV[1];
 open F, $ARGV[0] or die;
 while(<F>){
 	chomp;
@@ -57,28 +56,19 @@ for(@samples){
 	$count++;
 }
 
-open OUT, ">d3/Data.json" or die;
+open OUT, ">data.json" or die;
 my $nodeLine;
 my $edgeLine;
 my $numNodes = scalar %dists;
 my $num = 0;
 foreach my $sample (@samples) {
-	if (!exists($meta{$sample})){$meta{$sample} = $ct;}
-	if ($dists{$sample}{'dist'} <$maxDist){
 
-
-
-		$nodeLine .= "\t\t{\"id\": \"$sample\",\"group\":$meta{$sample}},\n";
-
-		$edgeLine .= "\t\t{\"source\": $samples{$sample}, \"target\": $samples{$dists{$sample}{'neighbour'}},\"value\":1},\n";
-	} else {
-		$nodeLine .= "\t\t{\"id\": \"$sample\",\"group\":$meta{$sample}},\n";
-
-	}
+	$nodeLine .= "\t\t{\"id\": \"$sample\",\"group\":$meta{$sample}},\n";
+	$edgeLine .= "\t\t{\"source\": $samples{$sample}, \"target\": $samples{$dists{$sample}{'neighbour'}},\"value\":$dists{$sample}{'dist'}},\n";
 	$num++;
 }
 
-print OUT "{\n\t\"graph\": [],\n\t\"nodes\":[\n";
+print OUT "var data = {\n\t\"graph\": [],\n\t\"nodes\":[\n";
 print OUT substr($nodeLine,0,length($nodeLine)-2);
 print OUT "],\n\t\"links\":[\n";
 print OUT substr($edgeLine,0,length($edgeLine)-2);
